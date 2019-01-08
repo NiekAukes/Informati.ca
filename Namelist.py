282fac65dd1276a3be1b162fd9bb30f00b54d5a7
Snotvlek <46380170+Snotvlek@users.noreply.github.com>
2019-01-08 10:13:58 +0100

    Nederlandse ABN vertaling van het cryptische gedoe van niek

--- Namelist.py
+++ Namelist.py
@@ -1,15 +1,16 @@
-names = input() #retrieves input
-seen = {} #declares dict.
-uniq = 0 #declares var
-namelist = names.split() #splits names into list
-_namelist = [] #declares parallel list
-for i in namelist: #retrieves all names seperately
-    if i not in seen: #if the name is not yet seen,
-        seen[i] = 0  #it creates a value for it
-        _namelist.append(i) #and appends it to the parallel list
-    seen[i] += 1 #increases the value for frequency
-print("name", "frequency")
-_namelist = sorted(_namelist) #sorts the list
-for i in _namelist: #takes all unique names individually
-    print(i, seen[i]) #prints the values
-print(uniq) #prints the uniqueness of the namelist
+names = input("Vul de namen in") #Vraagt om namen
+seen = {} #Maakt er een 'dictionary'(dubbele lijst) van. (Wachtwoorden en Namen)
+uniq = 0 #Declareert een variabele
+namelist = names.split() #Split namen naar een lijst
+_namelist = [] #Maakt een parallele lijst aan
+for i in namelist: #Vraagt alle namen apart van elkaar
+    if i not in seen: #Als de naam nog niet gezien is,
+        seen[i] = 0  #...creeërt het een value daarvoor
+        _namelist.append(i) #.. en voegt het aan de parallele lijst toe
+        uniq += 1;
+    seen[i] += 1 #Als het gezien is, voegt het 1 toe aan de frequentie van 'gezien', zodat het programma het herkent als gezien.
+print("name", "frequency") #print de naam en frequentie van die naam uit
+_namelist = sorted(_namelist) #sorteert de lijst
+for i in _namelist: #Neemt alle namen apart,
+    print(i, seen[i]) #print de frequentie van die naam uit,
+print(uniq) #... en print de uniekheid van die namen uit.
