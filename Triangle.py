c11e66c6350fa5143650d50a914a6e25cb4493b4
Snotvlek <46380170+Snotvlek@users.noreply.github.com>
2019-01-08 10:53:42 +0100

    Normale nederlandse vertaling geadd

--- Triangle.py
+++ Triangle.py
@@ -1,20 +1,20 @@
-abc = input()
-abc = abc.split()
-T = True #declares passage
+abc = input("Voer drie getallen in (a, b, c) voor a, b en c")               #Vraagt om een input voor abc
+abc = abc.split() #split de getallen
+T = True #Declareert een flag (signaal voor een functie of proces)
 for i in range(3):
-    abc[i] = int(abc[i]) #makes every value in abc an integer
+    abc[i] = int(abc[i]) #Maakt elke variabele in de lijst abc een integer
 for i in range(3):
-    if abc[i] >= abc[i-1] + abc[i-2]: #if one value is greater than the combined of the others, triangle isn't possible
-        print("Triangle not possible", abc[i], abc[i-1] + abc[i-2])
+    if abc[i] >= abc[i-1] + abc[i-2]: #Als één value groter is dan de combinatie van de andere twee, is een driehoek niet mogelijk.
+        print("Driehoek is niet mogelijk met de getallen", abc[i],", ", abc[i-1]," en ",abc[i-2])
         T = False
         break 
-if T:
-    print("Triangle Possible")
+if T: #als het wel mogelijk is om een driehoek te maken:
+    print("Driehoek mogelijk")
     for i in range(3):
-        if abc[i] == abc[i-1] == abc[i-2]: #searches for special triangle
-            print("this is a superspecial triangle")
+        if abc[i] == abc[i-1] == abc[i-2]: #Zoekt voor een speciale driehoek (alle zijden 60 graden)
+            print("Dit is een superspeciale driehoek :D")
             break
         if abc[i] == abc[i-1]:
-            print("this is a special triangle")
+            print("Dit is een gelijkbenige driehoek")
             break
 
