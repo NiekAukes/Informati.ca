cbdb04d532a6aca250da36758ece0686cf0ab432
Snotvlek <46380170+Snotvlek@users.noreply.github.com>
2019-01-08 10:20:45 +0100

    Update PascalTriangle.py

--- PascalTriangle.py
+++ PascalTriangle.py
@@ -1,11 +1,11 @@
 In = input()
 In = int(In)
 
-def T(_n, _k): #recursion method
+def T(_n, _k): #Herhalingsmethode
     if _k == 0 or _k == _n-1:
-        return 1 #base case
+        return 1 #Base Case
     else:
-        return T(_n-1, _k-1) + T(_n-1, _k) #recursion case
+        return T(_n-1, _k-1) + T(_n-1, _k) #Recursion Case
 for n in range(In): #counts for n in range of input
     nk = [] #declares empty list
     for k in range(n): #counts for k in range of n
