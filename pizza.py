e1d0498d0e9e17d0bd792df3ff66592808508400
Snotvlek <douwe@oscom.nl>
2019-01-14 17:56:52 +0100

    Infinite loops geadd (ook voor ingrediënten zodat je via een loopje al je ingrediënten kunt invoeren)

--- pizza.py
+++ pizza.py
@@ -1,39 +1,47 @@
-import math
+i = 1
+Ingredienten = "null"
 prijsIngredienten = 0 #In cent
 prijsIngredienten = float(prijsIngredienten)
 prijsPizza = 0  #In cent
 
-diameter = input("Voer de grootte van de pizza in (klein, medium, of groot).")
-Ingredienten = input("Wat voor ingredienten wil je erop?")
+while True:
+    diameter = input("Voer de grootte van de pizza in (klein, medium, of groot).")
+    if diameter == "klein": #Diameter beslissen aan de hand van de input
+        diameter = 20 #Diameter in cm
+        break
+    elif diameter == "medium":
+        diameter = 25
+        break
+    elif diameter == "groot":
+        diameter = 30
+        break
+    else:
+        print("Ongeldige grootte.")
+        continue
 
-if diameter == "klein": #Diameter beslissen aan de hand van de input
-    diameter = 20 #Diameter in cm
-elif diameter == "medium":
-    diameter = 25
-elif diameter == "groot":
-    diameter = 30
-else:
-    print("Ongeldige grootte.")
+while True:
+    if Ingredienten == "break":
+        break
+    else:
+        Ingredienten = input("Voer een ingrediënt in. Als je uit deze loop wil, moet je break intypen.")
+        i += 1
+        if "champignons" in Ingredienten:
+            prijsIngredienten += 1
+        if "ansjovis" in Ingredienten:
+            prijsIngredienten += 1.5
+        if "ham" in Ingredienten:
+            prijsIngredienten += 0.8
+        if "salami" in Ingredienten:
+            prijsIngredienten += 0.7
+        if "ui" in Ingredienten:
+            prijsIngredienten += 0.5
+        if "baguette" in Ingredienten:
+            prijsIngredienten += 10
+        continue
 
-Ingredienten = Ingredienten.split() #IngrediÃ«nten splitten in een list
-lijstIngredienten = ["champignons", "ansjovis", "ham", "salami", "ui", "paprika", "mosselen", "baguette"]
-for lijstIngredienten in Ingredienten:
-    if "champignons" in Ingredienten:
-        prijsIngredienten += 1
-    if "ansjovis" in Ingredienten:
-        prijsIngredienten += 1.5
-    if "ham" in Ingredienten:
-        prijsIngredienten += 0.8
-    if "salami" in Ingredienten:
-        prijsIngredienten += 0.7
-    if "ui" in Ingredienten:
-        prijsIngredienten += 0.5
-    if "baguette" in Ingredienten:
-        prijsIngredienten += 10
-
-prijsPizza = (((diameter / 2)**2) * math.pi) * 4 #Prijs in cent
-prijsIngredientenOppervlakte = prijsIngredienten + ((diameter /2) **2) * math.pi
+prijsPizza = (((diameter / 2)**2) * 3.14) * 4 #Prijs in cent
+prijsIngredientenOppervlakte = prijsIngredienten + ((diameter /2) **2) * 3.14
 
 prijsInCent = prijsIngredientenOppervlakte + prijsPizza
 
-print("De prijs van je pizza is ",int(prijsInCent / 100), "euro en ",int(prijsInCent % 100), "cent.")
+print("De prijs van je pizza is ",int(prijsInCent / 100), "euro en",int(prijsInCent % 100), "cent.")
