b370b2494dbba1854a57b23ed7edecea4b409781
Snotvlek <douwe@oscom.nl>
2019-01-20 19:29:43 +0100

    Update van alles

--- Namelist.py
+++ Namelist.py
@@ -1,15 +1,15 @@
-﻿names = input("Vul de namen in: ") 
-seen = {} 
-uniq = 0 
-namelist = names.split() 
-_namelist = [] 
-for i in namelist: 
-    if i not in seen: 
-        seen[i] = 0  
-        _namelist.append(i) 
+﻿names = input("Vul de namen in: ") #Vraagt om namen
+seen = {} #Maakt er een 'dictionary'(dubbele lijst) van. (Namen en Keren Gezien)
+uniq = 0 #Declareert een variabele
+namelist = names.split() #Split namen naar een lijst
+_namelist = [] #Maakt een parallele lijst aan
+for i in namelist: #Vraagt alle namen apart van elkaar
+    if i not in seen: #Als de naam nog niet gezien is,
+        seen[i] = 0  #...creeërt het een value daarvoor
+        _namelist.append(i) #.. en voegt het aan de parallele lijst toe
         uniq += 1
-    seen[i] += 1 
-_namelist = sorted(_namelist) 
-for i in _namelist: 
-    print(i,'-',seen[i]) 
-print("Er zijn", uniq,"verschillende namen.") 
+    seen[i] += 1 #Als het gezien is, voegt het 1 toe aan de frequentie van 'gezien', zodat het programma het herkent als gezien.
+_namelist = sorted(_namelist) #sorteert de lijst
+for i in _namelist: #Neemt alle namen apart,
+    print(i,'-',seen[i]) #print de frequentie van die naam uit,
+print("Er zijn", uniq,"verschillende namen.") #... en print de uniekheid van die namen uit.
--- nvm.py
+++ nvm.py
@@ -1,13 +1,11 @@
-x = 0
 try:
-    N = int(input("Enter a number for N between 0 and 24. This will be the beginning number of *s that the code will produce: "))
-    M = int(input("Enter a number for M between 0 and 60. This will be how man rows of *s there will be returned: "))
-    V = int(input("Enter a number for V between 0 and 5. This will be how many *s the code will output more than the previous line: "))
+    N = int(input("Kies een cijfer voor N tussen 0 en 24. Dit zijn het aantal sterretjes die bij de eerste rij te zien zijn."))
+    M = int(input("Kies een cijfer voor M tussen 0 en 60. Dit zijn het aantal rijen die te zien zullen zijn. "))
+    V = int(input("Kies een cijfer voor V tussen 0 en 5. Dit is het aantal sterretjes wat per rij groter wordt."))
 
     if N < 24 and N > 0 and M <60 and M > 0 and V >0 and V<5:
         for i in range (M):
-            print( N * "*" + x * V * "*")
-            x += 1
+            print( N * "*" + i * V * "*")
     else:
         print("The numbers were not in the specified range. Cancelled the action.")
 
