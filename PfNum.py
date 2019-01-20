dbd654b2dac94a53968e998ac95784ed19a872cf
NiekAukes <46373454+NiekAukes@users.noreply.github.com>
2019-01-20 19:41:09 +0100

    update

--- PfNum.py
+++ PfNum.py
@@ -1,11 +1,11 @@
 pfNum = input("vul een getal in: ")
 pfNum = int(pfNum)
-d = [] #Maakt lijst aan
-for i in range(pfNum-1): #Neemt elk nummer kleiner dan de input
-    Pfnum = float(pfNum) #Declareert een variabele gelijk aan de input
-    if float(pfNum/(i+1)) == Pfnum/(i+1): #Als het getal even is, is het een echte deler
-        d.append(i+1) #Voegt het nummer toe aan de delerlijst
-if sum(d) == pfNum: #Als de som van de delers gelijk is aan het getal, is het een perfect getal.
+d = [] 
+for i in range(pfNum-1): 
+    Pfnum = float(pfNum) 
+    if float(pfNum/(i+1)) == Pfnum/(i+1): 
+        d.append(i+1) 
+if sum(d) == pfNum: 
     print("Het nummer", pfNum , "is een perfect getal")
     print("Delers:")
     for i in d:
